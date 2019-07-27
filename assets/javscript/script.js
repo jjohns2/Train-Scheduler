f084e5100a75785616e259cff672ea744815bcbf
Jessica <JCOJOHNS2@GMAIL.COM>
2019-07-27 17:52:49 -0500

    html structure added with ids

---
+++ assets/javscript/script.js
@@ -0,0 +1,19 @@
+// Initialize Firebase
+//Button for adding Trains
+// Creates local "temporary" object for holding train data
+// Uploads train data to the database
+
+// Clears all of the text-boxes
+$("#train-input").val("");
+
+//Current Time
+
+//Difference between the times
+
+//Time apart (remainder)
+
+//Minute Until Train
+
+//Next Train
+
+//Add each train's data into the table
