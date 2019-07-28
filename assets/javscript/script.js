87f839bc38898dbc6b8cb2f05811521adb884407
Jessica <JCOJOHNS2@GMAIL.COM>
2019-07-27 19:31:46 -0500

    using the form will submit data to firebase

--- assets/javscript/script.js
+++ assets/javscript/script.js
@@ -1,10 +1,44 @@
 // Initialize Firebase
-//Button for adding Trains
-// Creates local "temporary" object for holding train data
+var config = {
+    apiKey: "AIzaSyBmIcNd-uONy9jvAi1_dwtao0ThrQNeMpI",
+    authDomain: "choo-choo-scheduler.com",
+    databaseURL: "https://choo-choo-scheduler.firebaseio.com",
+    storageBucket: "choo-choo-scheduler.appspot.com",
+    messagingSenderId: "742250943148",
+    projectID: "choo-choo-scheduler"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+//when the Button is Hit the Train is Added
+//takes in input
+$("#submitclick").click(function () {
+var trainName = $("#trainnameInput").val().trim();
+console.log(trainName);
+var destination = $("#destinationInput").val().trim();
+console.log(destination);
+var trainTime = $("#traintimeInput").val().trim();
+console.log(trainName);
+var Frequency = $("#freqInput").val().trim();
+console.log(Frequency);
+
 // Uploads train data to the database
+database.ref().push({
+    TrainName: trainName,
+    Destination: destination,
+    FirstTrainTime: trainTime,
+    Frequency: Frequency
+  });
 
 // Clears all of the text-boxes
-$("#train-input").val("");
+function clearData () {
+$("#trainnameInput, #destinationInput, #traintimeInput, #freqInput").val("");
+    return false;
+}
+clearData();
+
 
 //Current Time
 
